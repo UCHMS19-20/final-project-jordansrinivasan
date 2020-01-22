ed42ca57a3c60a7fba0a6d6dc77090535031fa45
Jordan <jsrinivasan@ucvts.org>
2020-01-22 14:52:57 -0500

    New code

--- src/pac-man.py
+++ src/pac-man.py
@@ -1,151 +1,64 @@
 import pygame, sys
-from pygame.locals import *
+from Character import Character
+from Constants import *
 
+class Pacman (Character):
+    def __init__(self):
+        self.radius = 6
+        self.color = YELLOW
+        self.surface = pygame.display.set_mode((588, 650), pygame.FULLSCREEN)
+        self.speed = 2
+        self.moveUp = self.moveDown = self.moveLeft = self.moveRight = False
+        self.score = 0
+        self.lives = 3
+        self.rect = pygame.Surface.get_rect()
+        self.rect.left = pygame.screen.get_width()/2
+        self.rect.top = pygame.screen.get_height()/2
+        self.direction = 0
 
-pygame.init()
-screen = pygame.display.set_mode((588, 650), pygame.FULLSCREEN)
-pygame.display.flip()
+    def reset (self):
+        self.radius = 6
+        self.color = YELLOW 
+        self.surface = pygame.display.set_mode((588, 650), pygame.FULLSCREEN)
+        self.rect = pygame.Surface.get_rect()
+        self.rect.left = pygame.screen.get_width()/2
+        self.rect.top = pygame.screen.get_width()/2
+        self.moveUp = self.moveDown = self.moveLeft = self.moveRight = False
+        self.direction = 0
 
-x_speed = 0
-y_speed = 0
+    def move (self, walls):
+        if self.moveUp and self.canMove (0, walls):
+            Character.move(self, 0)
+        elif self.moveLeft and self.canMove (1, walls):
+            Character.move (self, 1)
+        elif self.moveDown and self.canMove (2, walls):
+            Character.move (self, 2)
+        elif self.moveDown and self.canMove (3, walls):
+            Character.move (self, 3)
 
-class Coordinates:
-    def __init__ (self, x, y):
-        self.x = x
-        self.y = y
+    def teleport (self):
+        if self.rect.colliderect(pygame)
 
-free_to_move_left = False
-free_to_move_right = False
-free_to_move_up = False
-free_to_move_down = False
+    def getScoreSurface (self):
+        global YELLOW
+        return pygame.font.SysFont (None, 48). render ("Score: " + str (self.score), True, YELLOW)
 
-pac = Coordinates(screen.get_width()//2, screen.get_height()//2)
-pressed_up = False
-pressed_down = False
-pressed_left = False
-pressed_right = False
-while True:
-    bg = pygame.image.load("src/img/pacman.png")
-    screen.blit(bg, (0,0))
-    pacman = pygame.draw.circle(screen, (255, 255, 0), (pac.x, pac.y), 6, 0)    
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                pressed_up = True
-                free_to_move_up = True            
-            elif event.key == pygame.K_DOWN:
-                pressed_down = True
-                free_to_move_down = True
-            elif event.key == pygame.K_LEFT:
-                pressed_left = True
-                free_to_move_left = True
-            elif event.key == pygame.K_RIGHT:
-               pressed_right = True
-               free_to_move_right = True
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                pressed_up = False
-                free_to_move_up = True
-            elif event.key == pygame.K_DOWN:
-                pressed_down = False
-                free_to_move_down = True
-            elif event.key == pygame.K_LEFT:
-                pressed_left = False
-                free_to_move_left = True
-            elif event.key == pygame.K_RIGHT:
-                pressed_right = False
-                free_to_move_right = True
-    if pressed_left:
-        x_speed = -2
-        y_speed = 0
-    if pressed_right:
-        x_speed = 2
-        y_speed = 0
-   
-    if pressed_up:
-        y_speed = -2
-        x_speed = 0
-    if pressed_down: 
-        y_speed = 2
-        x_speed = 0
+    def getLivesSurface (self):
+        global YELLOW
+        surface = pygame.font.SysFont (None, 48). render ("Lives: ", True, YELLOW)
+        x = 110
+        for i in range (self.lives):
+            pygame.draw.circle(Pacman.surface, Pacman.color, ())
+            x += 25
+        return surface
 
+    def getWinningSurface (self):
+        global YELLOW
+        return pygame.font.SysFont (None, 72). render ("You Win!", True, YELLOW)
 
-    if pac.x > 650:
-        pac.x = 0
-        pac.y = pac.y
-    elif pac.x < 0:
-        pac.x = 650
-        pac.y = pac.y
-
-    rectangles_list = [
-        pygame.Rect(54, 54, 63, 44),
-        pygame.Rect(159, 54, 83, 44),
-        pygame.Rect(285, 0, 20, 98),
-        pygame.Rect(347, 54, 83, 44),
-        pygame.Rect(472, 54, 63, 44),
-        pygame.Rect(54, 139, 63, 20),
-        pygame.Rect(222, 139, 146, 20),
-        pygame.Rect(472, 139, 63, 20),
-        pygame.Rect(159, 139, 20, 146),
-        pygame.Rect(411, 139, 20, 146),
-        pygame.Rect(159, 202, 83, 20),
-        pygame.Rect(285, 139, 20, 83),
-        pygame.Rect(347, 202, 83, 20),
-        pygame.Rect(159, 326, 20, 83),
-        pygame.Rect(222, 390, 146, 20),
-        pygame.Rect(411, 326, 20, 83),
-        pygame.Rect(54, 452, 63, 20),
-        pygame.Rect(159, 452, 83, 20),
-        pygame.Rect(285, 390, 20, 83),
-        pygame.Rect(347, 452, 83, 20),
-        pygame.Rect(472, 452, 63, 20),
-        pygame.Rect(0, 515, 54, 20),
-        pygame.Rect(97, 452, 20, 84),
-        pygame.Rect(472, 452, 20, 84),
-        pygame.Rect(534, 515, 54, 20),
-        pygame.Rect(159, 515, 20, 83),
-        pygame.Rect(222, 515, 146, 20),
-        pygame.Rect(411, 515, 20, 83),
-        pygame.Rect(54, 578, 189, 20),
-        pygame.Rect(285, 515, 20, 83),
-        pygame.Rect(347, 578, 189, 20),
-        pygame.Rect(0, 0, 588, 12),
-        pygame.Rect(0, 0, 12, 212),
-        pygame.Rect(576, 0, 12, 212),
-        pygame.Rect(0, 202, 117, 83),
-        pygame.Rect(471, 202, 117, 83),
-        pygame.Rect(0, 326, 117, 83),
-        pygame.Rect(471, 326, 117, 83),
-        pygame.Rect(0, 409, 12, 241),
-        pygame.Rect(0, 638, 588, 12),
-        pygame.Rect(576, 409, 12, 241),
-        pygame.Rect(221, 264, 52, 11),
-        pygame.Rect(316, 264, 52, 11),
-        pygame.Rect(221, 264, 11, 83),
-        pygame.Rect(357, 264, 11, 83),
-        pygame.Rect(221, 336, 147, 11)
-    ]
-    
-    for rect in rectangles_list:
-        pygame.draw.rect(screen, (0, 255, 50), rect, -1)
-        if pacman.colliderect(rect):
-            if y_speed <= 0:
-                free_to_move_up = False
-            elif y_speed >= 0:
-                free_to_move_down = False
-            elif x_speed <= 0:
-                free_to_move_left = False
-            elif x_speed >= 0:
-                free_to_move_right = False
-    if free_to_move_left:
-        pac.x += x_speed 
-    elif free_to_move_right:
-        pac.x -= x_speed
-    elif free_to_move_up:
-        pac.y -= y_speed
-    elif free_to_move_down:
-        pac.y += y_speed
-
+    def getLosingSurface (self):
+        global YELLOW
+        return pygame.font.SysFont (None, 72). render ("You Lose...", True, YELLOW)
     
     pygame.display.flip()
 
@@ -452,12 +365,13 @@ while True:
         pygame.Rect(512, 616, 6, 6),
         pygame.Rect(533, 616, 6, 6)
     ]
-    for rect in food:
-        pygame.draw.rect(screen, (255, 255, 255), rect)  
+    for r in food:
+        pellet = pygame.draw.rect(screen, (255, 255, 255), r)  
+        if pacman.colliderect(r):
+            food.remove(r)
         pygame.display.flip()
-    if pacman.colliderect(rect):
-        food.remove(rect)
-        screen.blit (screen, (rect.x, rect.y))   
+
+         
     
         
 
